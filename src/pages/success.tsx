8fd03ee076c00ee8a5ae1613fd4df0a939a843a2
gtgambit <gtgambit@gmail.com>
2023-05-17 18:32:30 +0300

    success component fix

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
-
 import { useStateContext } from "../../context/StateContext";
 import { runFirework } from "../../lib/utils";
 
-const Success = () => {
+const Success: React.FC = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
   useEffect(() => {
@@ -24,7 +22,7 @@ const Success = () => {
           <BsBagCheckFill />
         </p>
         <h2>Thank you for your order!</h2>
-        <p className="email-msg"> Check your email inbox for the receipt.</p>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
         <p className="description">
           If you have any questions, please email us.
           <a className="email" href="mailto:order@example.com">
@@ -32,7 +30,7 @@ const Success = () => {
           </a>
         </p>
         <Link href="/">
-          <button type="button" width="300px" className="btn">
+          <button type="button" className="btn" style={{ width: "300px" }}>
             Continue Shopping
           </button>
         </Link>
