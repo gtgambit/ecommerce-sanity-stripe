a0430a8fbae7446055d1ad9100e4add473a50abb
gtgambit <gtgambit@gmail.com>
2023-05-17 17:20:40 +0300

    updates to getStripe types

--- lib/getStripe.ts
+++ lib/getStripe.ts
@@ -4,7 +4,13 @@ let stripePromise: Promise<Stripe | null>;
 
 const getStripe = (): Promise<Stripe | null> => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+    if (typeof stripePublishableKey === "string") {
+      stripePromise = loadStripe(stripePublishableKey);
+    } else {
+      // Handle the case when the key is not defined or not a string
+      stripePromise = Promise.resolve(null);
+    }
   }
   return stripePromise;
 };
